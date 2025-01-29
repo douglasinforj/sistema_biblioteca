73fb90840eb8d074da3c2de6c49736a73e2be6d2
Douglas Silva <douglasitpro@gmail.com>
2025-01-29 01:54:44 -0300

    refactor: ajustando erro de digitação

--- core/models.py
+++ core/models.py
@@ -20,7 +20,7 @@ class Emprestimo(models.Model):
     Livro = models.ForeignKey(Livro, on_delete=models.CASCADE, related_name='emprestimos')
     nome_usuário  = models.CharField(max_length=100)
     data_empresto = models.DateField(auto_now_add=True)
-    data_devolucao = models.DateField(null=True, black=True)
+    data_devolucao = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.nome_usuário} - {self.livro.titulo}"
