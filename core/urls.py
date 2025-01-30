ddacd5f667846d777e64a6e4be7bdbfb35d5f00f
Douglas Silva <douglasitpro@gmail.com>
2025-01-30 11:03:42 -0300

    feat: Adicionando rota url da view home

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('livros/', views.livros_list, name='livros_list'),
     path('emprestimos/', views.emprestimos_list, name='emprestimos_list')
 ]
