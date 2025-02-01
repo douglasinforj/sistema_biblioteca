487204dd6d4c383df51b7a0af413f5b58aa58d9f
Douglas Silva <douglasitpro@gmail.com>
2025-01-31 21:14:35 -0300

    feat: ajustando as rotas para livro_create, livro_update, livro_delete

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
     path('livros/', views.livros_list, name='livros_list'),
-    path('emprestimos/', views.emprestimos_list, name='emprestimos_list')
+    path('livros/novo', views.livro_create, name='livro_create'),
+    path('livros/<int:pk>/', views.livro_detail, name='livro_detail'),
+    path('livros/<int:pk>/editar/', views.livro_update, name='livro_update'),
+    path('livros/<int:pk/deletar/', views.livro_delete, name='livro_delete'),
+
+
+    path('emprestimos/', views.emprestimos_list, name='emprestimos_list'),
+    
 ]
