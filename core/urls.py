4b99408411e20120f52da03187af98dd80420bbb
Douglas Silva <douglasitpro@gmail.com>
2025-02-02 16:51:34 -0300

    Ajustando erro de rota

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('livros/novo', views.livro_create, name='livro_create'),
     path('livros/<int:pk>/', views.livro_detail, name='livro_detail'),
     path('livros/<int:pk>/editar/', views.livro_update, name='livro_update'),
-    path('livros/<int:pk/deletar/', views.livro_delete, name='livro_delete'),
+    path('livros/<int:pk>/delete/', views.livro_delete, name='livro_delete'),
 
 
     path('emprestimos/', views.emprestimos_list, name='emprestimos_list'),
