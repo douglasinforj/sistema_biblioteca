96037c6c3f7b6e9000810e1fab319e565de3dc1e
Douglas Silva <douglasitpro@gmail.com>
2025-02-02 17:32:43 -0300

    Ajuste na reta update

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('livros/', views.livros_list, name='livros_list'),
     path('livros/novo', views.livro_create, name='livro_create'),
     path('livros/<int:pk>/', views.livro_detail, name='livro_detail'),
-    path('livros/<int:pk>/editar/', views.livro_update, name='livro_update'),
+    path('livros/<int:pk>/update/', views.livro_update, name='livro_update'),
     path('livros/<int:pk>/delete/', views.livro_delete, name='livro_delete'),
 
 
