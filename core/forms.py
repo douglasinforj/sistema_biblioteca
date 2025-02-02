a51a89e9cead12de35e693162a785199b533aea5
Douglas Silva <douglasitpro@gmail.com>
2025-02-02 16:25:04 -0300

    Adicionando diretamente a form widgets para utlização no templates

--- core/forms.py
+++ core/forms.py
@@ -5,3 +5,9 @@ class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
         fields = ['titulo', 'autor', 'data_publicacao', 'disponivel']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'autor': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_publicacao': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'disponivel': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
