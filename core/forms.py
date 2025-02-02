96aee46a4235656133bc07e76249172edd2bbe1e
Douglas Silva <douglasitpro@gmail.com>
2025-02-02 16:42:01 -0300

    Ajustando o campo autor para ser Select, pois recebe dados foreignkey

--- core/forms.py
+++ core/forms.py
@@ -7,7 +7,7 @@ class LivroForm(forms.ModelForm):
         fields = ['titulo', 'autor', 'data_publicacao', 'disponivel']
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
-            'autor': forms.TextInput(attrs={'class': 'form-control'}),
+            'autor': forms.Select(attrs={'class': 'form-control'}),
             'data_publicacao': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'disponivel': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
